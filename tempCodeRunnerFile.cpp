e61555f19e1f12201108161b2856ac122d1355e2
Pongpx <yannafong555@gmail.com>
2024-02-09 14:20:57 +0700

    Finish

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 int main(){
 	int nA,nB;
-	int *A,*B, **C;	
+	int *A,*B,*C;	
 	
 	cout << "Length of A: ";
 	cin >> nA;
 	
-	A = new int[nA];
+	A = new int;
 	cout << "Input Array A: ";
 	for(int i=0; i < nA; i++) cin >> A[i];
 	
 	cout << "Length of B: ";
 	cin >> nB;
 	
-	B = new int[nB];
+	B = new int;
 	cout << "Input Array B: ";
 	for(int i=0; i < nB; i++) cin >> B[i];
 	
-	C = new int*[nA];
-	for(int i=0; i < nA; i++) C[i] = new int[nB];
+	C = new int;
+	for(int i=0; i < nA; i++) C[i] = new int;
 	
 	for(int i=0; i < nA; i++){
 		for(int j=0; j < nB; j++) {
-			C[i][j] = A[i]*B[j];
+			C = A[i]*B[j];
 		}
 	}
 	
@@ -39,10 +39,9 @@ int main(){
 		for(int j=0; j < nB; j++) {
 			cout << C[i][j] << "\t";
 		}
-		delete [] C[i];
 		cout << "\n";
 	}
-     delete [] A, delete [] B, delete [] C; 
+     delete A,B,C; 
 
 	return 0;
 }
